12eba2f32fdea9cd1ea56611831b3b7627aeeb14
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-03 12:22:49 -0700

    Optimizando codigo script

---
+++ components/deleteIcon.js
@@ -0,0 +1,15 @@
+const deleteIcon=()=>{
+    const content=`
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    const i=document.createElement("i");
+    i.classList.add("fas","fa-trash-alt","trashIcon","icon");
+    i.addEventListener("click",deleteTask);
+    return i
+}
+
+const deleteTask=(evento)=>{
+    const elemento =evento.target.parentElement;
+    elemento.remove();
+}
+
+export default deleteIcon;
--- script.js
+++ script.js
@@ -1,61 +1,31 @@
-( () => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
   
-  const btn =document.querySelector("[data-form-btn]");
 
-  const createTask= (evento)=>{
-    evento.preventDefault();
-    const input=document.querySelector("[data-form-input]");
-    const value=input.value;
-    const list=document.querySelector("[data-list]");
-    const task=document.createElement("li");
-    task.classList.add("card");
-    input.value=" ";
-    //content card
-    const taskContent=document.createElement('div');
-    const titleTask=document.createElement("span");
+const btn =document.querySelector("[data-form-btn]");
 
-
-    titleTask.classList.add('task');
-    titleTask.innerText=value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
+const createTask= (evento)=>{
+  evento.preventDefault();
+  const input=document.querySelector("[data-form-input]");
+  const value=input.value;
+  const list=document.querySelector("[data-list]");
+  const task=document.createElement("li");
+  task.classList.add("card");
+  input.value=" ";
   
-    //Agregar contenido
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-  };
+  //content card
+  const taskContent=document.createElement('div');
+  const titleTask=document.createElement("span");
+  titleTask.classList.add('task');
+  titleTask.innerText=value;
 
-  btn.addEventListener("click",createTask);
+  //Agregar contenido
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
+  list.appendChild(task);
+};
 
-  //Immediately invoked function expression IIFE
-  const checkComplete = ()=> {
-    const i=document.createElement("i");
-    i.classList.add("far","fa-check-square","icon");
-    i.addEventListener("click",completeTask);
-    return i
-  };
-
-  const completeTask=(event)=> {
-    const element=event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-  };
-
-  const deleteIcon=()=>{
-    const content=`
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    const i=document.createElement("i");
-    i.classList.add("fas","fa-trash-alt","trashIcon","icon");
-    i.addEventListener("click",deleteTask);
-    return i
-  }
-
-  const deleteTask=(evento)=>{
-    const elemento =evento.target.parentElement;
-    elemento.remove();
-  }
-
-})();
+btn.addEventListener("click",createTask);
 
