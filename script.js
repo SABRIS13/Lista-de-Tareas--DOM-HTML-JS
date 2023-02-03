ceb7a7a67e0f7f74849f253e1bde20fb62d42796
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-03 11:56:43 -0700

    Ocultar codigo a usuario

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const btn =document.querySelector("[data-form-btn]");
+( () => {
+  
+  const btn =document.querySelector("[data-form-btn]");
 
-const createTask= (evento)=>{
+  const createTask= (evento)=>{
     evento.preventDefault();
     const input=document.querySelector("[data-form-input]");
     const value=input.value;
@@ -10,31 +12,35 @@ const createTask= (evento)=>{
     input.value=" ";
     //content card
     const taskContent=document.createElement('div');
-    taskContent.appendChild(checkComplete());
+    
     const titleTask=document.createElement("span");
     titleTask.classList.add('task');
     titleTask.innerText=value;
+
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
+
     const content=`
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-//Agregar contenido
-  task.appendChild(taskContent);
-  list.appendChild(task);
+    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    //Agregar contenido
+    task.appendChild(taskContent);
+    list.appendChild(task);
+  };
 
-};
+  btn.addEventListener("click",createTask);
 
-btn.addEventListener("click",createTask);
+  //Immediately invoked function expression IIFE
+  const checkComplete = ()=> {
+    const i=document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click",completeTask);
+    return i
+  };
 
-const checkComplete = ()=> {
-  const i=document.createElement("i");
-  i.classList.add("far","fa-check-square","icon");
-  i.addEventListener("click",completeTask);
-  return i
-}
-
-const completeTask=(event)=> {
-  const element=event.target;
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon");
-  element.classList.toggle("far");
-}
+  const completeTask=(event)=> {
+    const element=event.target;
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+  };
+})();
