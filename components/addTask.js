1fd34dcdde3122f43e3ccd9c8797789753796933
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-07 15:51:12 -0700

    Organizar codigo en script funciones

--- components/addTask.js
+++ components/addTask.js
@@ -2,36 +2,39 @@ import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 
 export const addTask = (evento) => {
-  const list = document.querySelector('[data-list]');
-  const task = createTask(evento);
-  list.appendChild(task);
+    evento.preventDefault();
+
+    const list = document.querySelector('[data-list]');
+    const input = document.querySelector('[data-form-input]');
+    const calendar = document.querySelector('[data-form-date]');
+
+    const value = input.value;
+    const date = calendar.value;
+    const dateFormat = moment(date).format('DD/MM/YYYY');
+
+    input.value = '';
+    calendar.value='';
+
+    const taskObj = {
+        value,
+        dateFormat,
+    };
+
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push({taskObj});
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+
+    const task = createTask(taskObj);
+    list.appendChild(task);
 };
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  console.log(taskList);
-  const input = document.querySelector('[data-form-input]');
-  const calendar = document.querySelector('[data-form-date]');
-  const value = input.value;
-  const date = calendar.value;
-  const dateFormat = moment(date).format('DD/MM/YYYY');
-  console.log(dateFormat);
+const createTask = ({value,dateFormat}) => {
+
   const task = document.createElement('li');
   task.classList.add('card');
-  input.value = '';
-  //backticks
   const taskContent = document.createElement('div');
-
-  const taskObj = {
-    value,
-    dateFormat,
-  };
-
-  taskList.push(taskObj);
-
-  localStorage.setItem('tasks', JSON.stringify(taskList));
-
+  
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
   titleTask.innerText = value;
