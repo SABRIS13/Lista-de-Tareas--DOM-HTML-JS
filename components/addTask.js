21bc4cc5af43efb735fcf763d6eb2cf6506babb0
Sabrina Contreras <sabriscon13@hotmail.com>
2023-02-07 16:49:33 -0700

    Agrupando tareas por fecha, creación de archivo services

--- components/addTask.js
+++ components/addTask.js
@@ -1,50 +1,56 @@
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (evento) => {
-    evento.preventDefault();
+  evento.preventDefault();
 
-    const list = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]');
-    const calendar = document.querySelector('[data-form-date]');
+  const list = document.querySelector('[data-list]');
+  const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector('[data-form-date]');
 
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format('DD/MM/YYYY');
+  const value = input.value;
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
 
-    input.value = '';
-    calendar.value='';
+  if (value === '' || date === '') {
+    return;
+  }
 
-    const taskObj = {
-        value,
-        dateFormat,
-    };
+  input.value = '';
+  calendar.value = '';
 
+  const taskObj = {
+    value,
+    dateFormat,
+  };
 
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    taskList.push(taskObj);
-    localStorage.setItem('tasks', JSON.stringify(taskList));
+  list.innerHTML = '';
 
-    const task = createTask(taskObj);
-    list.appendChild(task);
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  taskList.push(taskObj);
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+
+  displayTasks();
 };
 
-export const createTask = ({value,dateFormat}) => {
-    
-    const task = document.createElement('li');
-    task.classList.add('card');
-    const taskContent = document.createElement('div');
-    
-    const titleTask = document.createElement('span');
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-  
-    const dateElement = document.createElement('span');
-    dateElement.innerHTML = dateFormat;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon());
-    return task;
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement('li');
+  task.classList.add('card');
+
+  const taskContent = document.createElement('div');
+
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+
+  const dateElement = document.createElement('span');
+  dateElement.innerHTML = dateFormat;
+
+  task.appendChild(taskContent);
+  task.appendChild(dateElement);
+  task.appendChild(deleteIcon());
+  return task;
 };
